1c04653d5d6bd8d54bcbdefc889503f788cb1fd2
Stewest <stew.west@amazeelabs.com>
2018-09-21 20:14:59 +0200

    Adds ToDo List component to Apo

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,16 +1,19 @@
-import React, { Component } from 'react';
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import TodoList from './components/TodoList';
 
 const App = () => (
   <div className="App">
     <header className="App-header">
       <img src={logo} className="App-logo" alt="logo" />
-      <h1 className="App-title">Welcome to React</h1>
+      <h1 className="App-title">TODOs</h1>
     </header>
     <p className="App-intro">
-      To get started, edit <code>src/App.js</code> and save to reload.
+      What do you need to do?
     </p>
+
+    <TodoList />
   </div>
 );
 
