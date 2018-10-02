3193192c0e7c9169fecb29ea4bb9d0f9c8e003e5
maitak <maitakambarami@gmail.com>
2018-10-02 10:58:07 +0200

    First attempt of Todo

---
+++ src/App.js
@@ -0,0 +1,55 @@
+import React, { Component } from 'react'
+import './App.css'
+import TodoList from './Components/TodoList'
+import TodoItems from './Components/TodoItems'
+
+class App extends Component {
+  constructor() {
+    super()
+    this.state = {
+      items: [],
+      currentItem: {text:'', key:''},
+    }
+  }
+  handleInput = e => {
+    const itemText = e.target.value
+    const currentItem = { text: itemText, key: Date.now() }
+    this.setState({
+      currentItem,
+    })
+  }
+  deleteItem = key => {
+    const filteredItems = this.state.items.filter(item => {
+      return item.key !== key
+    })
+    this.setState({
+      items: filteredItems,
+    })
+  }
+  addItem = e => {
+    e.preventDefault()
+    const newItem = this.state.currentItem
+    if (newItem.text !== '') {
+      console.log(newItem)
+      const items = [...this.state.items, newItem]
+      this.setState({
+        items: items,
+        currentItem: { text: '', key: '' },
+      })
+    }
+  }
+  render() {
+    return (
+      <div className="App">
+        <TodoList addItem={this.addItem}
+          inputElement={this.inputElement}
+          handleInput={this.handleInput}
+          currentItem={this.state.currentItem}
+        />
+        <TodoItems entries={this.state.items}deleteItem={this.deleteItem}/>
+      </div>
+    )
+  }
+}
+
+export default App
